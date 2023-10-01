bdbd8700ca8881e5eeb549a5c6d70e5faa388048
snehau259 <snehau259@gmail.com>
2023-10-01 16:46:01 +0530

    pages & routing

---
+++ pages/events/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Id() {
+  return (
+    <div>
+      <p>This is how you can fetch page using induvidual id</p>
+    </div>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <p>this is the events page</p>
+    </div>
+  )
+}
