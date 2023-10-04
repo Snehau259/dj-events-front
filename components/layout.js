93e015d442f8a1f799e59bd8cb271e8260a4d9fe
snehau259 <snehau259@gmail.com>
2023-10-04 19:32:45 +0530

    add. showcase

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import Showcase from './showcase'
 
 
 
@@ -14,6 +15,7 @@ export default function Layout({title,description,keywords,children}) {
     <meta name='keywords' content={keywords}/>
     </Head>
     <Header/>
+    <Showcase/>
     {children}
     <Footer/>
     </div>
---
+++ components/showcase.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '@/styles/showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+    <h1>Welcome to the part</h1>
+    <h2>Find the hottest DJ events</h2>
+      
+    </div>
+  )
+}
